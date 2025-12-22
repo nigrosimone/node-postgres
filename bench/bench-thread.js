427510ea0597aba776b92472d40b5b16c5a2f758
Nigro Simone <nigro.simone@gmail.com>
2025-12-22 07:05:56 +0100

    chore: clean up

--- bench/bench-thread.js
+++ bench/bench-thread.js
@@ -21,48 +21,45 @@ try {
   const pool = new Pool()
   const nativePool = new NativePool()
 
-  Promise.all([
-    pool.query('SELECT 1'), 
-    nativePool.query('SELECT 1')
-  ]).then(() => {
-    bench
-    .add(benchmark.name, async() => {
-      if( benchmark.native ){
-        return nativePool.query(benchmark.query)
-      }
-      return pool.query(benchmark.query)
-    })
-    .run()
+  Promise.all([pool.query('SELECT 1'), nativePool.query('SELECT 1')])
     .then(() => {
-      const task = bench.tasks[0]
-      // use throughput mean as Hz if not available on result
-      const hz = task.result.hz || task.result.throughput.mean
-      // use throughput rme
-      const rme = task.result.rme || task.result.throughput?.rme
-      // use latency samplesCount
-      const samples = task.result.latency.samplesCount
+      bench
+        .add(benchmark.name, async () => {
+          if (benchmark.native) {
+            return nativePool.query(benchmark.query)
+          }
+          return pool.query(benchmark.query)
+        })
+        .run()
+        .then(() => {
+          const task = bench.tasks[0]
+          // use throughput mean as Hz if not available on result
+          const hz = task.result.hz || task.result.throughput.mean
+          // use throughput rme
+          const rme = task.result.rme || task.result.throughput?.rme
+          // use latency samplesCount
+          const samples = task.result.latency.samplesCount
 
-      const formattedHz = hz.toLocaleString('en-US', { maximumFractionDigits: 0 })
-      const formattedRme = rme.toFixed(2)
+          const formattedHz = hz.toLocaleString('en-US', { maximumFractionDigits: 0 })
+          const formattedRme = rme.toFixed(2)
 
-      const output = `${task.name} x ${formattedHz} req/sec ±${formattedRme}% (${samples} runs sampled)`
-      parentPort.postMessage(output)
-     
-      return Promise.all([pool.end(), nativePool.end()])
+          const output = `${task.name} x ${formattedHz} req/sec ±${formattedRme}% (${samples} runs sampled)`
+          parentPort.postMessage(output)
+
+          return Promise.all([pool.end(), nativePool.end()])
+        })
+        .catch((err) => {
+          console.log(err)
+          parentPort.postMessage(`Error: ${err.message}`)
+          return Promise.all([pool.end(), nativePool.end()])
+        })
     })
     .catch((err) => {
       console.log(err)
       parentPort.postMessage(`Error: ${err.message}`)
       return Promise.all([pool.end(), nativePool.end()])
     })
-  }).catch((err) => {
-      console.log(err)
-      parentPort.postMessage(`Error: ${err.message}`)
-      return Promise.all([pool.end(), nativePool.end()])
-  })
-
 } catch (error) {
   console.log(error)
   parentPort.postMessage(`Error: ${error.message}`)
 }
-  
